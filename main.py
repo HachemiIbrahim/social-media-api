85563de2886ddc9fffe6eb7e7d24152cdec96171
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-04-09 02:10:46 +0100

    adding CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from auth import authentication
@@ -13,5 +14,15 @@ app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(comment.router)
 app.include_router(post.router)
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
